9b369fa9928d589f935c44a9035f74e5b4c7bd94
Anton Burmagin <burmagin.anton.s@gmail.com>
2024-12-08 22:54:45 +0300

    dev forth: implement Verifier class

---
+++ src/main/java/project/src/verifier/Verifier.java
@@ -0,0 +1,45 @@
+package project.src.verifier;
+
+import project.src.Order;
+import project.src.data.ColorType;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Verifier {
+    public boolean colorVerifier(String color) {
+        if (ColorType.values().toString().contains(color.trim().toUpperCase())) {
+            return true;
+        }
+        System.out.println("Incorrect color! Choose one of " + Arrays.toString(ColorType.values()));
+        return false;
+    }
+
+    public boolean nameVerifier(String name) {
+        char[] nameArr = name.toLowerCase().toCharArray();
+        for (char letter : nameArr) {
+            if (!((letter >= 'a' && letter <= 'z') || (letter >= 'а' && letter <= 'я')))
+                return false;
+        }
+        System.out.println("Incorrect name format! Name should contain only letters (English or Russian)!");
+        return true;
+    }
+
+    public boolean ageVerifier(Object age) {
+        if (age instanceof Integer) {
+            if ((int)age > 0 && (int)age < 31)
+                return true;
+        }
+        System.out.println("Incorrect age format! It must be Integer from 1 to 30!");
+        return false;
+    }
+
+    public boolean weightVerifier(Object weight) {
+        if (weight instanceof Double) {
+            if ((double)weight > 0 && (double)weight < 156)
+                return true;
+        }
+        System.out.println("Incorrect weight format! It must be Double from 0+ to 155 kg (world record for dogs)!");
+        return false;
+    }
+}
