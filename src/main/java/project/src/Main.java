97d1872b8e5ebaf592b716482c1daf0534c45452
Anton Burmagin <burmagin.anton.s@gmail.com>
2024-12-08 22:09:57 +0300

    dev third: add ColorType

--- src/main/java/project/src/Main.java
+++ src/main/java/project/src/Main.java
@@ -3,37 +3,41 @@ import project.src.animals.AbsAnimal;
 import project.src.data.AnimalTypes;
 import project.src.factory.Factory;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Factory factory = new Factory();
-//        AbsAnimal animal = factory.create(AnimalTypes.DOG);
-//        animal.setName("Bob");
-//        animal.setAge(1);
-//        animal.setWeight(18.5);
-//        animal.setColor("Pink");
-//        System.out.println(animal.toString());
-//        animal.say();
-
-
+        ArrayList<AbsAnimal> animal = new ArrayList<AbsAnimal>();
+        AbsAnimal pet1 = factory.create(AnimalTypes.DOG);
 
         String order;
         Order orderType = null;
-//      System.out.println(orderType.EXIT.toString());
-//        while (true) {
-//            System.out.println("What's your command (add, list, exit):");
-//            Scanner input = new Scanner(System.in);
-//            order = input.nextLine().trim().toUpperCase();
-//            System.out.println(order);
-//            if (order.equals(orderType.EXIT.toString())) {
-//                System.out.println("Got exit. Goodbye!");
-//                break;
-//            } else if (order.equals(orderType.ADD.toString())) {
-//                System.out.println("Got add");
-//            } else if (order.equals(orderType.LIST.toString())) {
-//                System.out.println("Got list");
-//            } else {
-//                System.out.println("Got nothing");
-//            }
-//        }
+        //AbsAnimal animal = factory.create(AnimalTypes.DOG);
+
+        while (true) {
+            System.out.println("What's your command " + Arrays.toString(Order.values()) + ":");
+            Scanner input = new Scanner(System.in);
+            order = input.nextLine().trim().toUpperCase();
+            System.out.println(order);
+            if (order.equals(Order.EXIT.toString())) {
+                System.out.println("Got exit. Goodbye!");
+                break;
+            } else if (order.equals(Order.ADD.toString())) {
+                System.out.println("Got add");
+            } else if (order.equals(Order.LIST.toString())) {
+                if (animal.isEmpty()) {
+                    System.out.println("Your list is empty! Try " + Order.ADD.toString() + " first!");
+                    continue;
+                }
+                for (AbsAnimal iterator : animal) {
+                    System.out.println(iterator.toString());
+                }
+            } else {
+                System.out.printf("%s command doesn't exist!\n", order);
+            }
+        }
     }
 }
---
+++ src/main/java/project/src/data/ColorType.java
@@ -0,0 +1,12 @@
+package project.src.data;
+
+public enum ColorType {
+    PINK,
+    BLACK,
+    WHITE,
+    EBONY,
+    GREEN,
+    WET_ASPHALT,
+    GINGER,
+    RAINBOW
+}
